582db24d9661791855728c622dfe0c89c3348429
Piasiu <pawel@piaskowy.net>
2022-06-23 19:11:00 +0200

    fix

--- src/Utils/Log.php
+++ src/Utils/Log.php
@@ -6,7 +6,12 @@ class Log
     /**
      * @var string
      */
-    private string $filePath;
+    private string $path;
+
+    /**
+     * @var string
+     */
+    private string $defaultFileName;
 
     /**
      * @var string
@@ -14,23 +19,32 @@ class Log
     private string $dateFormat;
 
     /**
-     * @param string $filePath
+     * @param string $path
+     * @param string $defaultFileName
      * @param string $dateFormat
      */
-    public function __construct(string $filePath, string $dateFormat = 'Y-m-d H:i:s')
+    public function __construct(string $path, string $defaultFileName = 'app.log', string $dateFormat = 'Y-m-d H:i:s')
     {
-        $this->filePath = $filePath;
-        $this->dateFormat = $filePath;
+        $this->path = realpath($path);
+        $this->defaultFileName = $defaultFileName;
+        $this->dateFormat = $dateFormat;
+
+        if (!file_exists($this->path))
+        {
+            mkdir($this->path, 0777, true);
+        }
     }
 
     /**
      * @param string $content
      * @param string $tag
+     * @param string|null $fileName
      */
-    public function write(string $content, string $tag = ''): void
+    public function write(string $content, string $tag = '', ?string $fileName = null): void
     {
+        $fileName = $fileName ?? $this->defaultFileName;
         $content = date($this->dateFormat).' '.$tag."\r\n".$content."\r\n";
-        file_put_contents($this->filePath, $content, FILE_APPEND|LOCK_EX);
+        file_put_contents($this->path.DIRECTORY_SEPARATOR.$fileName, $content, FILE_APPEND|LOCK_EX);
     }
 
     /**
